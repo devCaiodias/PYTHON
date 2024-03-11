b0de44a5abdfce2acbfdb4658c80fef38d717524
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-10 21:14:30 -0300

    raise - lançando exceções (erros)

--- tryeexcept/index.py
+++ tryeexcept/index.py
@@ -40,25 +40,46 @@ except -> ocorreu algum erro ao tentar executar
 # try, except, else e finally
 # https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
 
-try:
-    print('ABRIR O ARQUIVO')
-    8/0
-except ZeroDivisionError as e:
-    print('----------------------')
-    print(e.__class__.__name__)
-    print('----------------------')
-    print(e)
-    print('----------------------')
-    print('N pode dividir numero com zero')
+# try:
+#     print('ABRIR O ARQUIVO')
+#     8/0
+# except ZeroDivisionError as e:
+#     print('----------------------')
+#     print(e.__class__.__name__)
+#     print('----------------------')
+#     print(e)
+#     print('----------------------')
+#     print('N pode dividir numero com zero')
     
-except IndexError as erro:
-    print('IndexError')
+# except IndexError as erro:
+#     print('IndexError')
     
-except (NameError, ImportError):
-    print('NameError, ImportError')
+# except (NameError, ImportError):
+#     print('NameError, ImportError')
     
-else:
-    print('N deu erro')
+# else:
+#     print('N deu erro')
     
-finally:
-    print('FECHEI O ARQUIVO')
+# finally:
+#     print('FECHEI O ARQUIVO')
+    
+
+# raise - lançando exceções (erros)
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
+
+def n_pode_ser_zero(d):
+    if d == 0:
+        raise ZeroDivisionError('Vc está tentando dividir um numero por zero')
+        
+def deve_ser_int_float(n):
+    tipo_n = type(n)
+    if not isinstance(n, (float, int)):
+        raise TypeError(f'"{n}" . Deve ser int ou float. "{tipo_n.__name__}"')
+
+def dividir(n,a):
+    n_pode_ser_zero(a)
+    deve_ser_int_float(n)
+    deve_ser_int_float(a)
+    return n / a
+
+print(dividir('8',5))
