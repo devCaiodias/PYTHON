db02bd2213d69334fb57c99dee6ada408d030ec5
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-11 12:16:08 -0300

    Introdução aos packages (pacotes) em Python

---
+++ modulos/modulo3.py
@@ -0,0 +1,10 @@
+import moduloo3.m3
+from moduloo3 import m3
+from moduloo3.m3 import *
+from moduloo3.m3 import soma_do_modulo
+
+print(soma_do_modulo(1,2))
+print(moduloo3.m3.soma_do_modulo(1,2))
+print(m3.soma_do_modulo(1,2))
+print(variavel)
+print(nova_var)
---
+++ modulos/moduloo3/m3.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'variavel',
+    'nova_var',
+    # 'soma_do_modulo',
+]
+
+variavel = 'Alguma coisa '
+nova_var = 'OK'
+def soma_do_modulo(x,y):
+    return x + y
