380abd306fcfb111b4f563002e5a5780b8c24d66
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-12 10:42:41 -0300

    execicio de ordenar produtos

--- list/Listcomprehension.py
+++ list/Listcomprehension.py
@@ -28,11 +28,11 @@ produtos = [
     {'nome': 'p3', 'preco': 30, },
 ]
 
-# novos_produtos = [
-#     {**produto, 'preco': produto['preco'] * 1.05}
-#     if produto['preco'] > 20 else {**produto}
-#     for produto in produtos
-# ]
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
 
 # p(novos_produtos)
 
