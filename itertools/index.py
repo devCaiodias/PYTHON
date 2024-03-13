a01acc74c9e8d70a86f0ac147827b531d2bc337a
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-13 14:35:59 -0300

    Combinations, Permutations e Product - Itertools

---
+++ itertools/index.py
@@ -0,0 +1,24 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+
+from itertools import combinations, permutations, product
+
+def print_iter(itereitor):
+    print(*list(itereitor), sep='\n')
+    print()
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unisex'],
+    ['algodão', 'poliester'],
+]
+
+print_iter(combinations(pessoas, 2))
+print_iter(permutations(pessoas, 2))
+print_iter(product(*camisetas))
