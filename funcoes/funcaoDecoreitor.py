1be249d3830ee821e0ccfffec32e9af2a0c56bc1
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-13 09:54:30 -0300

    167. Decoradores com parâmetros

--- funcoes/funcaoDecoreitor.py
+++ funcoes/funcaoDecoreitor.py
@@ -3,25 +3,54 @@
 # Funções decoradoras são funções que decoram outras funções
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções.
-def criar_fuc(func):
-    def interna(*args, **kwargs):
-        print('Vou te decorar!')
-        for arg in args:
-            is_string(arg)
-        resultado = func(*args, **kwargs)
-        print(f'O seu resoltado foi {resultado}')
-        print('Ok, agora vc foi decorada')
-        return resultado
-    return interna
+# Decoradores são "Syntax Sugar" (Açúcar sintático)
 
-def inverte_string(string):
-    return string[::-1]
+# def criar_fuc(func):
+#     def interna(*args, **kwargs):
+#         print('Vou te decorar!')
+#         for arg in args:
+#             is_string(arg)
+#         resultado = func(*args, **kwargs)
+#         print(f'O seu resoltado foi {resultado}')
+#         print('Ok, agora vc foi decorada')
+#         return resultado
+#     return interna
 
-def is_string(param):
-    if not isinstance(param, str):
-        raise TypeError('param deve ser uma string')
+# @criar_fuc
+# def inverte_string(string):
+#     return string[::-1]
+
+# def is_string(param):
+#     if not isinstance(param, str):
+#         raise TypeError('param deve ser uma string')
 
 
-inverte_string_checando_parametro = criar_fuc(inverte_string)
-inverte = inverte_string_checando_parametro('125')
-print(inverte)
+# inverte = inverte_string('125')
+# print(inverte)
+
+
+# Decoradores com parâmetros
+
+def fabrica_de_decoradores(a = None, b = None, c = None):
+    def fabrica_de_func(func):
+        print('Decoradora 1')
+        
+        def aninhada(*args, **kwargs):
+            print(f'Parametros do decorador, {a,b,c}')
+            print('Aninhada')
+            res = func(*args, **kwargs)
+            return res
+        return aninhada
+    return fabrica_de_func
+
+@fabrica_de_decoradores(1,5,3)
+def soma(x,y):
+    return x + y
+
+decoradora = fabrica_de_decoradores()
+multiplicacao = decoradora(lambda x, y: x * y)
+
+dez_mais_cinco = soma(10,5)
+mutiplica = multiplicacao(5,10)
+print(dez_mais_cinco)
+print(mutiplica)
