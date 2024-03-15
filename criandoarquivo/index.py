2e1ff05d06c9cb0cbebc90a21c72de616368c07a
Caio Dias <caiodiasmartins12@gmail.com>
2024-03-15 14:13:52 -0300

    with open (context manager) e métodos úteis do TextIOWrapper

--- criandoarquivo/index.py
+++ criandoarquivo/index.py
@@ -26,6 +26,34 @@ caminho_arquivo += 'arquivo.txt'
 
 # arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Arquivo vai ser fechado')
+def escrever_com_quebra_de_linha(msg):
+    arquivo.write(f'{msg} \n')
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    escrever_com_quebra_de_linha('Caio lindo')
+    escrever_com_quebra_de_linha('papo')
+    arquivo.writelines(
+        ('Carro \n', 'Mota \n',)
+    )
+    arquivo.seek(0,0)
+    print(arquivo.read())
+    
+    print('-'*10)
+    
+    print('Lendo')
+    arquivo.seek(0,0)
+    print(arquivo.readline().strip())
+    print(arquivo.readline())
+    
+    print('-'*10)
+       
+    print('READLINES')
+    arquivo.seek(0,0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+    
+# print('-'*10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
